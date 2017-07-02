0bdf8cd641697921961eda1672949cf66b60c9e8
Michael Kirschbaum <mkirschbaum@car.fit>
2017-07-01 23:29:33 -0700

    add test case

--- spec/DeckSpec.js
+++ spec/DeckSpec.js
@@ -1,12 +1,17 @@
 describe("Deck", function() {
   var Deck = require('../lib/Deck');
+  var Card = require('../lib/Card');
   var deck;
 
   beforeEach(function() {
     deck = new Deck();
   });
-/*
-  it("case", function() {
-    expect("string").toEqual("string");
-  }); */
+
+  it("should be a standard size deck", function() {
+    expect(deck.getSize()).toEqual(64);
+  });
+
+  it("should be able to return a card", function() {
+    expect(typeof(deck.draw())).toBe("Card");
+  });
 });
