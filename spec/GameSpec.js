b9e56443505a11dd95a89c9a92e0fa52fb23049b
Michael Kirschbaum <mkirschbaum@car.fit>
2017-07-03 08:54:27 -0400

    add PlayerSpec.js

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -1,5 +1,6 @@
 describe("Game", function() {
   var Game = require('../lib/Game');
+  var Deck = require('../lib/Deck');
   var game;
 
   beforeAll(function() {
@@ -9,9 +10,15 @@ describe("Game", function() {
   });
 
   it("a deck should be evenly split among players", function() {
+    var numCards;
+
     game.getPlayers().forEach(function(player) {
-      
+      numCards += player.getCards().length;
     });
+
+    var deck = new Deck();
+
+    expect(numCards).toEqual(deck.getSize());
   });
 
   it("should allow more than two players", function() {
