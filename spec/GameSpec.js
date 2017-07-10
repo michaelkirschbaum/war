1e60df3ae0723100dc3f96f4ac69a1657586dbee
Michael Kirschbaum <mkirschbaum@car.fit>
2017-07-10 14:24:32 -0400

    add test case

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -18,4 +18,35 @@ describe("Game", function() {
       expect(player.numCards()).toEqual((new Deck(numPlayers)).size() / 4);
     });
   });
+
+  it("should throw an exception when play requires war", function() {
+    var [player1, player2] = game.getPlayers();
+
+    player1.add([new Card("Ace", "Spades")]);
+    player2.add([new Card("Ace", "Spades")]);
+
+    for (var i = 0; i < player1.numCards() - 1; i++)
+      player1.play();
+
+    for (var j = 0; j < player2.numCards() - 1; j++)
+      player2.play();
+
+    expect(function() {
+      game.play();
+    }).toThrowError("play requires war");
+  });
+
+  it("should be able to play a round", function() {
+    var winner = null;
+
+    while (winner == null) {
+      try {
+        winner = game.play();
+      } catch (e) {
+        game = new Game(numPlayers);
+      }
+    }
+
+    // expect();
+  });
 });
